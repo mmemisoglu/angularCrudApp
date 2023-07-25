92485825abf412fc32a59235a9563e7497973998
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-07-25 10:15:24 +0300

    Printing the posted form to Json file

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -15,7 +17,11 @@ export class EmpAddEditComponent {
     'Yüksek Lisans'
   ];
 
-  constructor(private _fb: FormBuilder){
+  constructor(
+    private _fb: FormBuilder,
+    private _empService:EmployeeService,
+    private _dialogRef:DialogRef<EmpAddEditComponent>
+    ){
     this.empForm = this._fb.group({
       firstName: '',
       lastName: '',
@@ -32,7 +38,15 @@ export class EmpAddEditComponent {
 
   onFormSubmit(){
     if(this.empForm.valid){
-      console.log(this.empForm.value)
+      this._empService.addEmployee(this.empForm.value).subscribe({
+        next: (val: any) => {
+          alert('Çalışan başarıyla eklendi.')
+          this._dialogRef.close();
+        },
+        error: (err: any) => {
+          console.error(err);
+        },
+      })
     }
   }
 
