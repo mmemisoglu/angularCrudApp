e5b20f768abb91a37bdf44ed164553d88b114b85
Mustafa Memişoğlu <m.memisoglu04@gmail.com>
2023-07-25 14:44:05 +0300

    Edit&Delete Button

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { EmployeeService } from '../services/employee.service';
-import { DialogRef } from '@angular/cdk/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-emp-add-edit',
   templateUrl: './emp-add-edit.component.html',
   styleUrls: ['./emp-add-edit.component.css']
 })
-export class EmpAddEditComponent {
+export class EmpAddEditComponent implements OnInit {
   empForm: FormGroup;
   education: string[] = [
     'Lise',
@@ -20,7 +20,8 @@ export class EmpAddEditComponent {
   constructor(
     private _fb: FormBuilder,
     private _empService:EmployeeService,
-    private _dialogRef:DialogRef<EmpAddEditComponent>
+    private _dialogRef:MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
     ){
     this.empForm = this._fb.group({
       firstName: '',
@@ -32,21 +33,38 @@ export class EmpAddEditComponent {
       company: '',
       experience: '',
       package: '',
+
     })
     
+  }
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data)
   }
 
   onFormSubmit(){
     if(this.empForm.valid){
-      this._empService.addEmployee(this.empForm.value).subscribe({
-        next: (val: any) => {
-          alert('Çalışan başarıyla eklendi.')
-          this._dialogRef.close();
-        },
-        error: (err: any) => {
-          console.error(err);
-        },
-      })
+      if(this.data){
+        this._empService.updateEmployee(this.data.id, this.empForm.value).subscribe({
+          next: (val: any) => {
+            alert('Çalışan başarıyla güncellendi.')
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }else{
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            alert('Çalışan başarıyla eklendi.')
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        })
+      }
+    
     }
   }
 
